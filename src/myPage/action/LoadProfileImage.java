dd20fdf3761a9f1a8b0fbe14e16aff9c0211df03
rldhks8745 <sdaff1004@gmail.com>
2018-06-03 18:12:37 +0900

    0603 mypage controller, action 추가

---
+++ src/myPage/action/LoadProfileImage.java
@@ -0,0 +1,20 @@
+package myPage.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import action.Action;
+import action.ActionForward;
+
+public class LoadProfileImage implements Action
+{
+
+	@Override
+	public ActionForward execute( HttpServletRequest request, HttpServletResponse response )
+	{
+		ActionForward forward = new ActionForward();
+		
+		return null;
+	}
+
+}
